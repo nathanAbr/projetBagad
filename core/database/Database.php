09f3b96c18213f2949689828cd1ae9c6a52ad73d
nathanABR <nathan.abraham@outlook.fr>
2017-04-11 09:27:18 +0200

    project architecture

---
+++ core/database/Database.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Nathan
+ * Date: 11/04/2017
+ * Time: 09:15
+ */
+class Database
+{
+
+}
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Nathan
+ * Date: 11/04/2017
+ * Time: 09:14
+ */
