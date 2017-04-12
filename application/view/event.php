41d0132f487d83df9ce08299a9ffb036bae0c597
nathanABR <nathan.abraham@outlook.fr>
2017-04-12 08:57:59 +0200

    add view for exemple

--- application/view/event.php
+++ application/view/event.php
@@ -1,9 +1,10 @@
-<?php
-foreach($event as $item){ ?>
-    <div class="event">
-        <h1><?php echo $item['nom']; ?></h1>
-        <p>
-            <?php echo $item['description']; ?>
-        </p>
-    </div>
+<section class="event-section">
+<?php foreach($event as $item){ ?>
+        <div class="event">
+            <h1><?php echo $item['nom']; ?></h1>
+            <p>
+                <?php echo $item['description']; ?>
+            </p>
+        </div>
 <?php } ?>
+</section>
