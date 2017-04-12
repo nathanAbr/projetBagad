1ea8aefdf46f82f5d9d3d86a1904d50220d6e318
orlando1108 <erwan.raulo2015@campus-eni.fr>
2017-04-12 15:01:22 +0200

    modif event

--- application/controller/Event.php
+++ application/controller/Event.php
@@ -8,8 +8,8 @@ class Event extends \core\controller\Controller
 	
 	public function show_event()
 	{
-		$event = new \application\model\Event();
-        $data['event'] = $event->getAllEvent();
+		$event = new \application\model\Evenement_Model();
+        $data['event'] = $event->loadEvent()();
         $this->loadView('event', $data);
 	}
 
