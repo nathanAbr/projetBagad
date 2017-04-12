c290f3c3535a39a3e3c39562e5d8806b02dedcf6
orlando1108 <erwan.raulo2015@campus-eni.fr>
2017-04-12 11:23:42 +0200

    ajout models

---
+++ application/model/Actualite_Model.php
@@ -0,0 +1,58 @@
+<?php 
+
+namespace application\model;
+class Actualite_Model Extends \core\model\Model { 
+    
+    
+    function loadActu() { 
+	    $tableau = array();
+	    $i = 0;
+        $reponse = $bd->query('SELECT idActualite, titre, image, description, date, m.nom, m.prenom 
+								FROM actualite 
+								INNER JOIN membre m ON fk_membre = m.idMembre');
+
+		$donnees = $reponse->fetchAll();
+		$reponse->closeCursor(); 
+		return $donnees ;
+    } 
+	
+	function insertActu($Actualite) { 
+       
+    $req = $bd->prepare('INSERT INTO actualite(titre, image, description, date, fk_membre) VALUES(:titre, :image, :description, :date, :idMembre)');
+	$req->bindParam(':titre',$Actualite['titre']);
+	$req->bindParam(':image',$Actualite['image']);
+	$req->bindParam(':description',$Actualite['description']);
+	$req->bindParam(':date',$Actualite['date']);
+	$req->bindParam(':idMembre',$Actualite['idMembre']);
+	$req->execute();
+
+		if($req){
+			
+			echo 'Le jeu a bien été ajouté !';
+		}
+    }
+	
+	function updateActu($Actualite) { 
+	
+		$req = $bd->prepare('UPDATE actualite SET titre="'.$Actualite['titre'].'", image="'.$Actualite['image'].'",
+										   description="'.$Actualite['description'].'", date="'.$Actualite['date'].'", fk_membre = "'.$Actualite['idMembre'].'"  
+							  WHERE idActualite='.$Actualite['id']);
+		$req->execute();
+		if($req){
+			echo 'Le jeu a bien été ajouté !';
+		}
+    } 
+	function deleteActu($Actualite) { 
+		$req = $bd->prepare('DELETE FROM actualite WHERE titre ="'.$Actualite['titre'].'"');
+		$req->execute();
+		if($req){
+			echo 'Le jeu a bien été supprimé !';
+		}
+    } 
+} 
+
+
+
+
+
+?>
---
+++ application/model/Membre_Model.php
@@ -0,0 +1,69 @@
+<?php 
+namespace application\model;
+
+class Membre_Model Extends \core\model\Model { 
+    
+    
+    function loadMembre() { 
+	    $tableau = array();
+	    $i = 0;
+        $reponse = $bd->query('SELECT m.idMembre, m.mail, m.nom, m.prenom, m.dateNaissance, m.telephone, m.login, m.motDePasse,
+										i.nom as "instrument", a.rue1 as "rue", l.ville , l.pays, l.codePostale, g.libelle 
+								FROM membre m 
+								INNER JOIN instrument i ON m.fk_instrument = i.idInstrument 
+								INNER JOIN adresse a ON m.fk_adresse = a.idAdresse 
+								INNER JOIN localite l ON a.fk_localite = l.idLocalite 
+								INNER JOIN groupe g ON m.fk_groupe = g.idGroupe');
+
+		$donnees = $reponse->fetchAll();
+		$reponse->closeCursor(); 
+		return $donnees;
+    } 
+	
+	function insertMembre($Membre) { 
+        
+    $req = $bd->prepare('INSERT INTO membre(mail, nom, prenom, dateNaissance, telephone, login, motDePasse, fk_instrument, fk_adresse, fk_groupe)
+					      VALUES(:mail, :nom, :$donnees, :dateNaissance, :telephone, :login, :motDePasse, :fk_instrument ,:fk_adresse, :fk_groupe)');
+	$req->bindParam(':mail',$Membre['mail']);
+	$req->bindParam(':nom',$Membre['nom']);
+	$req->bindParam(':prenom',$Membre['prenom']);
+	$req->bindParam(':dateNaissance',$Membre['dateNaissance']);
+	$req->bindParam(':telephone',$Membre['telephone']);
+	$req->bindParam(':login',$Membre['login']);
+	$req->bindParam(':motDePasse',$Membre['motDePasse']);
+	$req->bindParam(':fk_instrument',$Membre['fk_instrument']);
+	$req->bindParam(':fk_adresse',$Membre['fk_adresse']);
+	$req->bindParam(':fk_groupe',$Membre['fk_groupe']);
+	$req->execute();
+
+		if($req){
+			
+			echo 'Le jeu a bien été ajouté !';
+		}
+    }
+	
+	function updateMembre($Membre) { 
+	
+		$req = $bd->prepare('UPDATE membre SET mail="'.$Membre['mail'].'", nom="'.$Membre['nom'].'", prenom="'.$Membre['prenom'].'", dateNaissance="'.$Membre['dateNaissance'].'"
+							                       telephone="'.$Membre['telephone'].'",login="'.$Membre['login'].'",motDePasse="'.$Membre['mdp'].'",fk_instrument="'.$Membre['idInstrument'].'",
+												   fk_adresse="'.$Membre['adresse'].'",fk_groupe="'.$Membre['groupe'].'"
+							  WHERE id='.$Membre['id']);
+		$req->execute();
+		if($req){
+			echo 'Le jeu a bien été mis à jour !';
+		}
+    } 
+	function deleteMembre() { 
+		$req = $bd->prepare('DELETE FROM membre WHERE nom ="'.$Membre['nom'].'" AND prenom = "'.$Membre['prenom'].'"');
+		$req->execute();
+		if($req){
+			echo 'Le jeu a bien été supprimé !';
+		}
+    } 
+} 
+
+
+
+
+
+?>
