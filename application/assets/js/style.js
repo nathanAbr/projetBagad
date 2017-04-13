3625358fb1084e594664e290039af6dd0b39a88d
orlando1108 <erwan.raulo2015@campus-eni.fr>
2017-04-13 16:38:06 +0200

    event style css

--- application/assets/js/style.js
+++ application/assets/js/style.js
@@ -6,3 +6,55 @@ for(var i = 0; i<element.count;i++){
         element[i].classList.push("right");
     }
 }
+
+
+// Get the modal
+var modal = document.getElementById("modal");
+var createForm = document.getElementById("createForm");
+var deleteForm = document.getElementById("deleteForm");
+var updateForm = document.getElementById("updateForm");
+/*var modalDelete = document.getElementById('modalDelete');
+var modalUpdate = document.getElementById('modalUpdate');*/
+// Get the button that opens the modal
+var btnCreate = document.getElementById("btnCreate");
+var listButtonDelete = document.getElementsByClassName("btnDelete");
+var listButtonUpdate = document.getElementsByClassName("btnUpdate");
+//var btnUpdate = document.getElementById("btnUpdate");
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+/*var spanDelete = document.getElementsByClassName("close")[1];
+var spanUpdate = document.getElementsByClassName("close")[2];*/
+// When the user clicks the button, open the modal 
+btnCreate.onclick = function() {
+    modal.style.display = "block";
+    createForm.style.display = "block";
+}
+
+for( item of listButtonDelete){
+    item.onclick = function(){
+        modal.style.display = "block";
+        deleteForm.style.display = "block";
+    }
+}
+
+for( item of listButtonUpdate){
+    item.onclick = function(){
+        modal.style.display = "block";
+        updateForm.style.display = "block";
+    }
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+    createForm.style.display = "none";
+    deleteForm.style.display = "none";
+    updateForm.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
--- application/view/event.php
+++ application/view/event.php
@@ -1,10 +1,153 @@
 <section class="event-section">
-<?php foreach($event as $item){ ?>
+
+<?php
+//var_dump($_SESSION['users'][0]['prenom']);
+ if($_SESSION['users'][0]['groupe'] == 'bureau'){?>
+  <button id="btnCreate" class="btnEdit, btnCreate" type="button">Ajouter un évènement</button>
+ 
+ 
+ 
+<div id="modal" class="modal">
+
+  <!-- Modal content -->
+  <div class="modal-content">
+    <span class="close">&times;</span>
+   
+
+<div>
+
+  <form id="createForm" class="form" action="/action_page.php">
+  <h3>Ajoutez un évènement</h3>
+    <label for="nom">Nom</label>
+    <input type="text" id="nom" name="nomEvent" placeholder="Nom de l'évènement">
+
+	<div class ="dateBlock">
+	<div class ="date" >
+    <label for="dateDebut">Debut de l'évènement</label>
+    <input type="date" id="dateDebut" name="debutEvent" placeholder="Debut">
+	</div>
+	<div class ="date">
+	<label for="dateFin">Fin de l'évènement</label>
+    <input type="date" id="dateFin" name="finEvent" placeholder="Fin">
+	</div>
+	</div>
+
+	<label for="cachet">Montant du cachet</label>
+    <input type="text" id="cachet" name="cachetEvent" placeholder="Montant">
+	
+	<label for="cachet">Description de l'évènement</label>
+    <textarea type="text" id="cachet" name="descriptionEvent" placeholder="Description"></textarea>
+	
+	<label for="image">Image de l'évènement</label>
+	<input type="file" accept="image/*"  value="Image" id="image" name="imageEvent">
+	
+	
+    <h3>Lieu</h3>
+    <input type="text"  id="rue1" name="rue1Event" placeholder="Rue N°1">
+	<input type="text"  id="rue2" name="rue2Event" placeholder="Rue N°2">
+	<input type="text"  id="ville" name="villeEvent" placeholder="Ville">
+	<input type="text"  id="cp" name="cpEvent" placeholder="Code postal">
+	<input type="text"  id="pays" name="paysEvent" placeholder="Pays">
+     
+	
+	<h3>Type de d'évènement</h3>
+    <select id="type" name="typeEvent">
+      <option value="australia">Sortie</option>
+      <option value="canada">Concours</option>
+      <option value="usa">Réunion</option>
+    </select>
+	
+	<h3>Associez un référent exterieur (organisateur)</h3>
+    <input type="text"  id="nomRef" name="nomRefEvent" placeholder="Nom">
+	<input type="text"  id="prenomRef" name="prenomRefEvent" placeholder="Prenom">
+	<input type="text"  id="telRef" name="telRefEvent" placeholder="N° téléphone">
+	<input type="email"  id="mailRef" name="mailRefEvent" placeholder="Adresse mail">
+	
+	
+	<label for="createur">Createur de l'évènement</label>
+	<input type="text" id="createur" name="createurEvent" value="<?php echo $_SESSION['users'][0]['prenom']; ?>">
+  
+    <input type="submit" class="submit" value="Enregistrer">
+  </form>
+  
+  <form id="deleteForm" class="form" action="/action_page.php">
+  <h3>Supprimez l'évènement</h3>
+    <label>Etes-vous certain de vouloir supprimer cet évènement ?</label>
+    <input type="text" id="nom" name="nomEvent" placeholder="Nom de l'évènement">
+
+    <input type="submit" class="submit" value="Oui">
+	<input type="submit" class="submit" value="Non">
+  </form>
+  
+  <form id="updateForm" class="form" action="/action_page.php">
+  <h3>Mettez à jour l'évènement</h3>
+    <label for="nom">Nom</label>
+    <input type="text" id="nom" name="nomEvent" placeholder="Nom de l'évènement">
+	
+    <div class ="dateBlock">
+	<div class ="date">
+    <label for="dateDebut">Debut de l'évènement</label>
+    <input type="date" id="dateDebut" name="debutEvent" placeholder="Debut">
+	</div>
+	<div class ="date">
+	<label for="dateFin">Fin de l'évènement</label>
+    <input type="date" id="dateFin" name="finEvent" placeholder="Fin">
+	</div>
+	</div>
+
+	<label for="cachet">Montant du cachet</label>
+    <input type="text" id="cachet" name="cachetEvent" placeholder="Montant">
+	
+	<label for="description">Description de l'évènement</label>
+    <textarea id="description" name="descriptionEvent" placeholder="Description"></textarea>
+	
+	<label for="image">Image de l'évènement</label>
+	<input type="file" accept="image/*"  value="Image" id="image" name="imageEvent">
+	
+   <h3>Lieu</h3>
+    <input type="text"  id="rue1" name="rue1Event" placeholder="Rue N°1">
+	<input type="text"  id="rue2" name="rue2Event" placeholder="Rue N°2">
+	<input type="text"  id="ville" name="villeEvent" placeholder="Ville">
+	<input type="text"  id="cp" name="cpEvent" placeholder="Code postal">
+	<input type="text"  id="pays" name="paysEvent" placeholder="Pays">
+	
+	<h3>Type d'évènement</h3>
+    <select id="type" name="typeEvent">
+      <option value="australia">Sortie</option>
+      <option value="canada">Concours</option>
+      <option value="usa">Réunion</option>
+    </select>
+	
+	<h3>Référent exterieur (organisateur)</h3>
+    <input type="text"  id="nomRef" name="nomRefEvent" placeholder="Nom">
+	<input type="text"  id="prenomRef" name="prenomRefEvent" placeholder="Prenom">
+	<input type="text"  id="telRef" name="telRefEvent" placeholder="N° téléphone">
+	<input type="email"  id="mailRef" name="mailRefEvent" placeholder="Adresse mail">
+	
+	<label for="createur">Createur de l'évènement</label>
+	<input type="text" id="createur" name="createurEvent" value="">
+	
+	
+	<input type="submit" class="submit" value="Mettre à jour">
+  </form>
+</div>
+  </div>
+
+</div>
+<?php } ?>
+
+
+
+<?php 
+foreach($event as $item){ ?>
         <div class="event">
             <h1><?php echo $item['eventNom']; ?></h1>
+			<span> Date: du <?php echo date('d M Y', strtotime($item['dateDebut'])); ?> au <?php echo date('d M Y', strtotime($item['dateFin'])); ?></span>
             <p>
-                <?php echo $item['description']; ?>
+               <?php echo $item['description']; ?>
             </p>
+			<button id="" class="btnEdit, btnDelete" type="button">Supprimer un évènement</button>
+			<button id="" class="btnEdit, btnUpdate" type="button">Modifier un évènement</button>
         </div>
 <?php } ?>
 </section>
